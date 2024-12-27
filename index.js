61b332091013317d2f0880e7f19bf1ed9afb871d
emilazer <emilragimov270@gmail.com>
2024-12-27 10:26:49 +0400

    Update index.js

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ app.listen(port, () => {
 });
 
 const statusMessages = ["Listening To AzeCp "];
-const statusTypes = [ 'dnd', 'idle'];
+const statusTypes = [ 'dnd'];
 let currentStatusIndex = 0;
 let currentTypeIndex = 0;
 
